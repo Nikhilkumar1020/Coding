7f83efcce759520e457e25328036b6efa138580c
Nikhilkumar1020 <nikhilofficial1020@gmail.com>
2026-08-07 14:33:25 +0530

    Time: 0 ms (100%), Space: 15 MB (23.14%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> indexOf;
+        for(int idx = 0; idx < (int)nums.size(); ++idx){
+            int complement = target - nums[idx];
+
+            auto it = indexOf.find(complement);
+            if(it != indexOf.end()) {
+                return {it->second, idx};
+            }
+
+            indexOf[nums[idx]] = idx;
+        }
+        return {};
+    }
+};
