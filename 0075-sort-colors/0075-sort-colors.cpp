e04146645474debe9aa77327c4d2a8804a74b039
Nikhilkumar1020 <nikhilofficial1020@gmail.com>
2026-08-19 06:27:18 +0530

    Time: 0 ms (100%), Space: 11.6 MB (48.9%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -6,17 +6,17 @@ class Solution {
         int high = nums.size() - 1;
 
         while (mid <= high) {
-            if (nums[mid] == 0) {
+            if (nums[mid] == 2) {
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+            else if (nums[mid] == 0) {
                 swap(nums[low], nums[mid]);
                 low++;
                 mid++;
             }
-            else if (nums[mid] == 1) {
-                mid++;
-            }
             else {
-                swap(nums[mid], nums[high]);
-                high--;
+                mid++;
             }
         }
     }
