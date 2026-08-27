8ab8b265e23d2be570004c2eaf3ed37d7777d8d5
Nikhilkumar1020 <nikhilofficial1020@gmail.com>
2026-08-27 20:09:21 +0530

    Time: 0 ms (100%), Space: 9.8 MB (77.34%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+      int left = 0;
+      int right = s.length() - 1;
+
+      while (left < right) {
+        while (left < right && !isalnum(static_cast<unsigned char>(s[left]))) {
+            ++left;
+        }
+        while (left < right && !isalnum(static_cast<unsigned char>(s[right]))) {
+            --right;
+        }
+
+        if (tolower(static_cast<unsigned char>(s[left])) != tolower(static_cast<unsigned char>(s[right]))) {
+            return false;
+        }
+
+        ++left;
+        --right;
+      }   
+
+      return true;
+    }
+};
